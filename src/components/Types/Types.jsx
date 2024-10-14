585f40cf47a0a5ffac779ae67448729b51fd6e5b
AbhayBamboriya <bamboriya09@gmail.com>
2024-10-15 02:58:06 +0530

    Changes the alignment

--- src/components/Types/Types.jsx
+++ src/components/Types/Types.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 import PokemonList from "../Pokemonlist/PokemonList";
 import Search from "../Search/Search";
 import PokemonType from "./PokemonType";
-
+import { useNavigate } from "react-router-dom";
+// import '../Pokedex/Pokedex.css'
+import '../Pokedex/Pokedex.css'
 function Types(type){
     // const [pokemon]=useTypes('water',false)
     const [searchTerm,setSearchTerm]=useState('')
-
+    const navigate=useNavigate()
     // console.log('to be render',pokemon);
     return (    
 
         <div className="pokedex-wrapper">  
-        <button onClick={()=>navigate('/types')}><span>Search By Type</span></button>
-        <Search updateSearchTerm={setSearchTerm} placeholder={'Enter Type of Pokemon'}/>
+        
+        <div className="search">
+            <Search updateSearchTerm={setSearchTerm} placeholder={'Enter Type of Pokemon'}/>
+            <button onClick={()=>navigate('/')} className="btn"><span>Search By Name</span></button>
+        </div>
         {/* {searchTerm} */}
         {/* whenever the key in pokemonDeetails changs then rerendering will take place
             so for rerendering only key prop is used */}
